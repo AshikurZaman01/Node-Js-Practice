d7bbab11d8cd433ae5e6dd33aa896e70db11e45e
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-24 00:16:05 +0600

    updated

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
+const player = require('./servser.js')
 
-const path = require('path');
-const currentDir = __dirname;
-const dataFolder = 'dataCenter';
-
-const filePathNew = path.join(currentDir, dataFolder , 'dd.csv')
-
-console.log(filePathNew)
+console.log(player)
---
+++ servser.js
@@ -0,0 +1,6 @@
+
+const players = ['shakib', 'mim', 'tamim', 'mashrafe', 'mahmudullah', 'sakib', 'raihan', 'mustafizur', 'mashrafe', 'mahmudullah', 'sakib', 'raihan', 'mustafizur'];
+
+module.exports = {
+    players
+}
