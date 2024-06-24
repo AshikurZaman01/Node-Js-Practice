d382611a51a41d6c4a5c5c7db5f5e6ad07df9ced
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-24 15:59:55 +0600

    add append file system

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 const fs = require('fs');
 
-fs.writeFileSync('myText.txt' , 'ranna korar lok nai')
+fs.writeFileSync('myText.txt' , 'ranna korar lok nai. ')
+fs.appendFileSync('myText.txt' , 'biye korbo na')
