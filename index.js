7832e34cf996d879fb044f5baac3401c2c8a9697
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-24 15:41:59 +0600

    add basename

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const player = require('./servser.js')
 
-console.log(player)
+const path = require('path');
+
+const myPath = 'H:\Intern Projects\Node Js Practice\servser.js';
+
+console.log("fileName ", path.basename(myPath))
