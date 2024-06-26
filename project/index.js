1816e91f9269eb870d32b7803ad0ec475c879272
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-26 12:13:30 +0600

    data copy

--- project/index.js
+++ project/index.js
@@ -1,28 +1,13 @@
 
+
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const ourReadStream = fs.createReadStream(__dirname + '/bigData.txt', 'utf8');
+const ourWriteStream = fs.createWriteStream(__dirname + '/bigDataCopy.txt');
 
-    if (req.url === '/') {
 
-        res.write('<html><head><title>Form Data</title></head>')
-        res.write('<body><form action="/about" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
-        res.end();
-
-    } else if (req.url === '/about' && req.method === 'POST' ) {
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            res.write(chunk);
-            res.write('About Page');
-            res.end();
-        })
-
-    } else {
-        res.write('Page Not Found');
-        res.end();
-    }
-
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
+    console.log('new chunk received');
 })
-server.listen(3000);
-
-console.log('Listening to port 3000');
