2afea925746e04ca3ac91f77268cd589a8f35ba8
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-26 12:14:19 +0600

    data copy with pipe

--- project/index.js
+++ project/index.js
@@ -7,7 +7,5 @@ const ourReadStream = fs.createReadStream(__dirname + '/bigData.txt', 'utf8');
 const ourWriteStream = fs.createWriteStream(__dirname + '/bigDataCopy.txt');
 
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
-    console.log('new chunk received');
-})
+
+ourReadStream.pipe(ourWriteStream)
