a65c7595769b1cc8df4609432807f04268d06f6e
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-26 12:08:07 +0600

    add read stream

--- project/index.js
+++ project/index.js
@@ -1,9 +1,28 @@
 
+const http = require('http');
 
-const fs = require('fs');
+const server = http.createServer((req, res) => {
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf-8')
+    if (req.url === '/') {
 
-ourReadStream.on('data', (chunk) => {
-    console.log(chunk);
+        res.write('<html><head><title>Form Data</title></head>')
+        res.write('<body><form action="/about" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
+        res.end();
+
+    } else if (req.url === '/about' && req.method === 'POST' ) {
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            res.write(chunk);
+            res.write('About Page');
+            res.end();
+        })
+
+    } else {
+        res.write('Page Not Found');
+        res.end();
+    }
+
 })
+server.listen(3000);
+
+console.log('Listening to port 3000');
