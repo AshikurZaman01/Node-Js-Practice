8a69f75e5bb4511b990f4931a0de01dd2c69106b
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-30 01:46:22 +0600

    updated

--- express/index.js
+++ express/index.js
@@ -1,53 +1,27 @@
 
+
 const express = require('express');
+const { property } = require('lodash');
 const app = express();
+const cookieParser = require('cookie-parser');
 const port = 3000;
-
-let users = [
-    { id: 1, name: 'John' },
-    { id: 2, name: 'Jane' },
-    { id: 3, name: 'Bob' },
-    { id: 4, name: 'Alice' },
-    { id: 5, name: 'Mike' },
-]
-
-
 app.use(express.json());
+app.use(cookieParser());
 
-app.route('/users/:userID')
-    .get((req, res) => {
+const adminRoute = express.Router();
+adminRoute.get('/', (req, res) => {
+    console.log(req.path);
+    res.send('admin route');
+})
 
-        const userID = parseInt(req.params.userID);
-        const user = users.find(user => user.id === userID);
-        if (user) {
-            res.send(user);
-        } else {
-            res.status(404).send('User not found');
-        }
-    })
-    .put((req, res) => {
-        const userId = parseInt(req.params.userId);
-        const userIndex = users.findIndex(u => u.id === userId);
-        if (userIndex !== -1) {
-            users[userIndex] = { id: userId, ...req.body };
-            res.send(users[userIndex]);
-        } else {
-            res.status(404).send({ error: 'User Not Found' });
-        }
-    })
-    .delete((req, res) => {
-        const userId = parseInt(req.params.userId);
-        const userIndex = users.findIndex(u => u.id === userId);
-        if (userIndex !== -1) {
-            users = users.filter(u => u.id !== userId);
-            res.send({ message: 'User Deleted' });
-        } else {
-            res.status(404).send({ error: 'User Not Found' });
-        }
-    });
+app.use('/admin', adminRoute);
 
 
+app.get('/users/:id', (req, res) => {
+    console.log(req.cookies);
+    res.send('Hello World!');
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`server is running at http://localhost:${port}`);
 })
