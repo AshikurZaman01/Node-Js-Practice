d6cfaf60dfb85a8c82ce7e57d2ca9fb204538ab8
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-29 16:34:23 +0600

    add username

--- express/index.js
+++ express/index.js
@@ -1,35 +1,34 @@
 
 const express = require('express');
 const app = express();
-
 const port = 3000;
 
+const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+    { id: 4, name: 'David' },
+    { id: 5, name: 'Eve' },
+    { id: 6, name: 'Frank' },
+]
 
-const users = {
-    1: { name: "Alice", age: 40, address: 'khulna' },
-    2: { name: "Bob", age: 20, address: 'Dhaka' },
-    3: { name: "Charlie", age: 30, address: "Rajshahi" },
-    4: { name: "David", age: 25, address: 'barishal' },
-    5: { name: "Eve", age: 35, address: 'chittagong' },
-}
 
-app.param('userId', (req, res, next, userId) => {
+app.param('userName', (req, res, next, userName) => {
 
-    const user = users[userId];
+    const user = users.find(user => user.name.toLowerCase() === userName.toLowerCase());
 
     if (user) {
-        console.log(`Found user: ${user.name}`);
         req.user = user;
         next();
     } else {
-        res.status(404).send(`User with ID ${userId} not found.`);
+        res.status(404).json({ error: 'User not found' });
     }
 })
 
-app.get('/users/:userId', (req, res) => {
-    res.send(req.user);
+app.get('/users/:userName', (req, res) => {
+    res.json(req.user);
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`)
 })
