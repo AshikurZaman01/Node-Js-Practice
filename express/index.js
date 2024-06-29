f7ee0e19a9fa2d7626e7e034452499c9bf8019b4
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-29 15:58:29 +0600

    search userId

--- express/index.js
+++ express/index.js
@@ -1,20 +1,35 @@
 
 const express = require('express');
 const app = express();
-const admin = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+const users = {
+    1: { name: "Alice", age: 40, address: 'khulna' },
+    2: { name: "Bob", age: 20, address: 'Dhaka' },
+    3: { name: "Charlie", age: 30, address: "Rajshahi" },
+    4: { name: "David", age: 25, address: 'barishal' },
+    5: { name: "Eve", age: 35, address: 'chittagong' },
+}
+
+app.param('userId', (req, res, next, userId) => {
+
+    const user = users[userId];
+
+    if (user) {
+        console.log(`Found user: ${user.name}`);
+        req.user = user;
+        next();
+    } else {
+        res.status(404).send(`User with ID ${userId} not found.`);
+    }
 })
 
-app.use('/admin', admin);
-
-admin.get('/', (req, res) => {
-    res.send('Hello Admin!');
+app.get('/users/:userId', (req, res) => {
+    res.send(req.user);
 })
-
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
