b8213445ade7137b63c7464d87f9f1e299eaeaf7
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-01 00:09:51 +0600

    updated

--- file2.js
+++ file2.js
@@ -1,6 +1,8 @@
 
 const fs = require('fs');
 
-const writeFile = fs.writeFileSync(__dirname + "/text.txt", "Hello World Bangladesh");
+const content = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.';
+
+const writeFile = fs.writeFileSync(__dirname + "/text.txt", content);
 
 console.log(writeFile);
