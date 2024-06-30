b20bc571ebad338d0a2d745f4ae2a09ffeb18026
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-01 01:21:10 +0600

    add stream

--- file1.js
+++ file1.js
@@ -1,16 +1,18 @@
 
+
 const fs = require('fs');
 
+const readableStream = fs.createReadStream(__dirname + '/text.txt', 'utf8');
 
-fs.readFile(__dirname + '/text.txt', 'utf8', (err, data) => {
-    if (err) {
-        throw new Error('Error reading file');
+readableStream.on('data', (chunk) => {
+    console.log('New chunk received:');
+
+    const writeStream = fs.createWriteStream(__dirname + '/wtiteTxt.txt', 'utf-8');
+
+    if (writeStream) {
+        writeStream.write(chunk);
+    } else {
+        throw new Error('Could not open the file for writing');
     }
 
-    fs.writeFile(__dirname + '/wtiteTxt.txt', data, (err) => {
-        if (err) {
-            throw new Error("Error lag");
-        }
-    })
 })
-
