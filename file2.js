76378a87d89e3b11f7e194c1166f652fb770aab5
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-01 00:16:54 +0600

    add readFile async

--- file2.js
+++ file2.js
@@ -1,8 +1,13 @@
 
 const fs = require('fs');
 
-const content = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.';
+const filePath = __dirname + '/text.txt';
 
-const writeFile = fs.writeFileSync(__dirname + "/text.txt", content);
-
-console.log(writeFile);
+const readFile = fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log(data);
+    }
+})
